488f6cc28bd7a77a6960c8d93e1486590d6eb234
HariPatnam <patnamhari5432@gmail.com>
2023-03-06 22:51:12 +0530

    first commit

---
+++ hari/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def hari(request):
+    return HttpResponse('i want a job is important in life')
+
+def suri(request):
+    return HttpResponse('i want a job is important in life')
+def venu(request):
+    return HttpResponse('younger brother of hari')
